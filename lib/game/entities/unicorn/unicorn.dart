bf256b1eae98cbeb8582822ae5a2df60d86cbbc5
Alexis Choupault <alexis_choupault@connect-tech.sncf>
2023-09-13 17:18:32 +0200

    fix uniccorn & associated tests | run ok

--- lib/game/entities/unicorn/unicorn.dart
+++ lib/game/entities/unicorn/unicorn.dart
@@ -1,6 +1,7 @@
 import 'package:bullet_train/game/entities/unicorn/behaviors/tapping_behavior.dart';
 import 'package:bullet_train/gen/assets.gen.dart';
 import 'package:flame/components.dart';
+import 'package:flame/sprite.dart';
 import 'package:flame_behaviors/flame_behaviors.dart';
 import 'package:flutter/material.dart';
 
@@ -8,12 +9,12 @@ class Unicorn extends PositionedEntity with HasGameRef {
   Unicorn({
     required super.position,
   }) : super(
-          anchor: Anchor.center,
-          size: Vector2.all(32),
-          behaviors: [
-            TappingBehavior(),
-          ],
-        );
+    anchor: Anchor.center,
+    size: Vector2.all(32),
+    behaviors: [
+      TappingBehavior(),
+    ],
+  );
 
   @visibleForTesting
   Unicorn.test({
@@ -22,9 +23,13 @@ class Unicorn extends PositionedEntity with HasGameRef {
   }) : super(size: Vector2.all(32));
 
   SpriteAnimation? _animation;
+  late SpriteAnimationTicker _animationTicker;
 
   @visibleForTesting
   SpriteAnimation get animation => _animation!;
+
+  @visibleForTesting
+  SpriteAnimationTicker get animationTicker => _animationTicker;
 
   @override
   Future<void> onLoad() async {
@@ -37,25 +42,27 @@ class Unicorn extends PositionedEntity with HasGameRef {
         loop: false,
       ),
     );
-
+    final animationComponent =
+    SpriteAnimationComponent(animation: _animation, size: size);
+    _animationTicker = animationComponent.animationTicker!;
     resetAnimation();
-    animation.onComplete = resetAnimation;
+    _animationTicker.onComplete = resetAnimation;
 
-    await add(SpriteAnimationComponent(animation: _animation, size: size));
+    await add(animationComponent);
   }
 
   /// Set the animation to the first frame by tricking the animation
   /// into thinking it finished the last frame.
   void resetAnimation() {
-    animation
+    _animationTicker
       ..currentIndex = _animation!.frames.length - 1
       ..update(0.1)
       ..currentIndex = 0;
   }
 
   /// Plays the animation.
-  void playAnimation() => animation.reset();
+  void playAnimation() => _animationTicker.reset();
 
   /// Returns whether the animation is playing or not.
-  bool isAnimationPlaying() => !animation.isFirstFrame;
+  bool isAnimationPlaying() => !_animationTicker.isFirstFrame;
 }
