fae0e0e5b41c1b8958d2ae6955e0c76a697edfc2
Jocelyn Griselle <jocelyn.griselle@icloud.com>
2023-10-15 22:32:10 +0200

    feat : remove KeyboardHandler

--- lib/game/component/bullet_train_game.dart
+++ lib/game/component/bullet_train_game.dart
@@ -4,12 +4,9 @@ import 'package:bullet_train/game/component/background_component.dart';
 import 'package:bullet_train/game/component/train_component.dart';
 import 'package:flame/camera.dart';
 import 'package:flame/components.dart';
-import 'package:flame/events.dart';
 import 'package:flame/game.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter/widgets.dart';
 
-class BulletTrainGame extends FlameGame with HasKeyboardHandlerComponents {
+class BulletTrainGame extends FlameGame {
   BulletTrainGame({required this.theme});
 
   final GameTheme theme;
@@ -37,26 +34,5 @@ class BulletTrainGame extends FlameGame with HasKeyboardHandlerComponents {
     );
 
     await addAll([_world, camera]);
-  }
-
-  @override
-  KeyEventResult onKeyEvent(
-    RawKeyEvent event,
-    Set<LogicalKeyboardKey> keysPressed,
-  ) {
-    if (keysPressed.contains(LogicalKeyboardKey.arrowUp)) {
-      trainComponent.moveTo(Direction.top);
-      return KeyEventResult.handled;
-    } else if (keysPressed.contains(LogicalKeyboardKey.arrowDown)) {
-      trainComponent.moveTo(Direction.bottom);
-      return KeyEventResult.handled;
-    } else if (keysPressed.contains(LogicalKeyboardKey.arrowLeft)) {
-      trainComponent.moveTo(Direction.left);
-      return KeyEventResult.handled;
-    } else if (keysPressed.contains(LogicalKeyboardKey.arrowRight)) {
-      trainComponent.moveTo(Direction.right);
-      return KeyEventResult.handled;
-    }
-    return super.onKeyEvent(event, keysPressed);
   }
 }
